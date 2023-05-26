6a349807e402fc7b908793af58b7ee42502f1ff7
sathiyaramesh <sathiyaramesh1620@gmail.com>
2023-05-26 17:32:39 +0530

    ok

---
+++ ES6-2/stringliteral.js
@@ -0,0 +1,30 @@
+// Assiging Variables
+let firstName = "Hema"
+let lastname = "Kishore"
+let city = "Krishnagiri"
+
+console.log(firstName + " " + lastname + " lives in " + city)
+
+// console.log(firstName + lastname + " lives in " + city)
+
+//String interpolation
+console.log(`${firstName} ${lastname} lives in ${city}`)
+
+//multiline
+let msg = `Happy
+Birthday`
+
+console.log(msg)
+
+// let msg = "happy
+// birthday"(not possible)
+
+
+//single & double quotes in string
+msg = `Cat's name is "Persuian"`
+
+console.log(msg)
+
+// msg = 'Cat's name is Persuian
+// msg = "'Cat's name is Persuian"
+// msg = "Cat's name is Persuian"
