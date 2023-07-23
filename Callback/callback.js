d3dc867651c13c2518a93a70c125200c733a7530
sathiyaramesh <sathiyaramesh1620@gmail.com>
2023-07-23 10:48:14 +0530

    ok

--- Callback/callback.js
+++ Callback/callback.js
@@ -1,9 +1,32 @@
 let employee=[
     {id:"101",name:"sathiya",email:"sathiya@gmail.com"},
-    {id:"101",name:"ramesh",email:"ramesh@gmail.com"},
+    {id:"102",name:"ramesh",email:"ramesh@gmail.com"},
 
 ]
-let createEmployee=()=>{
+let createEmployee=(emp)=>{
+    setTimeout(()=>{
+        employee.push(emp)
+    },3000)
 
 }
-let getEmployee=()=>{}
+let getEmployee=()=>{
+    setTimeout(()=>{
+
+let row=""
+employee.forEach((emp)=>{
+  return  row=row+`<tr>
+    <td>${emp.id}</td>
+    <td>${emp.name}</td>
+    <td>${emp.email}</td>
+
+    </tr>`
+
+})
+
+
+        document.getElementById('data').innerHTML=row
+    },2000)
+}
+createEmployee({id:"103",name:"harish",email:"rocksha@gmail.com"},
+)
+getEmployee()
